5364281c102b47f4b6a8e960271f6bd5023bddc2
srinath730 <Srinathreddy96@gmail.com>
2025-06-05 12:41:53 +0530

    git checkot

--- vars/Git_checkout.groovy
+++ vars/Git_checkout.groovy
@@ -1,6 +1,5 @@
-def git_checkout(String repo_url, String branch) {
+def call(String repo_url, String branch) {
 	echo "Cloning repo from ${repo_url}"
-		checkout([
-			$class: '$GITSCM', branches: [[name: branch]], userRemoteConfigs: [[ url: repo_url ]]
-		])
+		checkout scmGit(branches: [[name: branch]],
+    userRemoteConfigs: [[url: repo_url]])
 }
